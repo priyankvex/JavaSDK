36b3e10906132757ce1afa2889ac6b6d09e6054b
egimaben <egimaben@gmail.com>
2016-04-19 12:54:34 +0300

    Added features for JavaSDK-1.2

--- src/main/java/io/cloudboost/CloudApp.java
+++ src/main/java/io/cloudboost/CloudApp.java
@@ -1,9 +1,17 @@
 package io.cloudboost;
 
 
+import io.cloudboost.push.CloudRegistrationService;
+import io.cloudboost.push.CloudPreferences;
 import io.cloudboost.util.CloudSocket;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter.Listener;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 /**
  * An abstract representation of CloudBoost, it manages connection parameters, URLs and authentication 
  * to the CloudBoost server
@@ -11,7 +19,7 @@ import io.socket.emitter.Emitter.Listener;
  *
  */
 
-public class CloudApp {
+public class CloudApp{
 	public static String appId;
 	public static String appKey;
 	public static String serverUrl = "https://api.cloudboost.io";
@@ -22,6 +30,10 @@ public class CloudApp {
 	public static String SESSION_ID=null;
 	public static String masterKey=null;
 	public static String socketIoUrl=null;
+	public static Activity context;
+	public static String SENDER_ID;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG="CloudApp";
 	
 	/**
 	 * gives the App ID to connect to
@@ -96,7 +108,6 @@ public class CloudApp {
 			
 			@Override
 			public void call(Object... args) {
-				System.out.println("disconnected");
 				
 			}
 		});
@@ -106,5 +117,44 @@ public class CloudApp {
 		CloudApp.appId = appId;
 		CloudApp.appKey = appKey;
 		CloudSocket.init(getSocketUrl());
+		
 	}	
+	public static void init(String appId, String appKey,Activity context,String _senderId) {
+		CloudApp.appId = appId;
+		CloudApp.appKey = appKey;
+		CloudApp.context=context;
+		CloudApp.SENDER_ID=_senderId;
+		CloudSocket.init(getSocketUrl());
+		if(checkPlayServices()){
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(context, CloudRegistrationService.class);
+            intent.putExtra(CloudPreferences.REFRESH, false);
+            context.startService(intent);
+		}
+		
+	}	
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private static boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(context);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                Log.i(TAG, "This device is not supported, but resolvable.");
+
+                apiAvailability.getErrorDialog(context, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i(TAG, "This device has no support for GCM.");
+            }
+            Log.i(TAG, "google play services not available.");
+
+            return false;
+        }
+        return true;
+    }
 }
