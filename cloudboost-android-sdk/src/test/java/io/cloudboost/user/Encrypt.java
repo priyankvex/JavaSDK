c88e1756d1ba5e0dd575fa5eb73a9b7c52444501
egimaben <egimaben@gmail.com>
2016-05-06 16:02:23 +0300

    tried spliting usertest

---
+++ cloudboost-android-sdk/src/test/java/io/cloudboost/user/Encrypt.java
@@ -0,0 +1,44 @@
+package io.cloudboost.user;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import io.cloudboost.CloudException;
+import io.cloudboost.CloudObject;
+import io.cloudboost.CloudObjectCallback;
+import io.cloudboost.CloudUser;
+import io.cloudboost.PrivateMethod;
+import io.cloudboost.UTIL;
+
+public class Encrypt {
+	void initialize() {
+		UTIL.init();
+	}
+	@Test(timeout = 20000)
+	public void encryptUserPassword() throws CloudException {
+		initialize();
+		final String username = PrivateMethod._makeString();
+		final String passwd = "abcd";
+		CloudUser user = new CloudUser();
+		user.setUserName(username);
+		user.setPassword(passwd);
+		user.setEmail(PrivateMethod._makeString() + "@abc.com");
+		user.save(new CloudObjectCallback() {
+
+			@Override
+			public void done(CloudObject newUser, CloudException e)
+					throws CloudException {
+				if (e != null) {
+					Assert.fail(e.getMessage());
+				}
+
+				if (newUser != null) {
+					if (newUser.get("password").equals(passwd)) {
+						Assert.fail("Password is not encrypted");
+					}
+				}
+			}
+		});
+	}
+}
